6d099ca334efd33e53040732602cd6ffa87631f2
Dragan <dragan1809989@gmail.com>
2020-08-08 15:20:21 +0200

    JOI is working

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const router = express.Router();
+
 router.use(express.json());
+
 
 
 router.get('/', function (req, res) {
   console.log("WORKS BE");
   res.send("WORKS FE");
-})
+});
 
 
 router.post('/', function (req, res) {
-  console.log(req.body.weight);
-  res.send("aaaaaaaaaaaaaaaaaaaa");
-})
+  const Joi = require('joi');
+  const schema = Joi.object({
+     weight: Joi.number().min(10).max(110).required()
+  });
+
+  if (schema.validate(req.body).error) {
+    return res.status(400).send("weight walue should be between 10 and 100 kg")
+  }
+  res.send("OK");
+});
 
 module.exports = router;
